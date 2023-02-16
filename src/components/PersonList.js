b8cd00182706b0ebe32a6e5f3617c6bec394c694
d1huang <109196554+d1huang@users.noreply.github.com>
2023-02-16 19:18:45 +0000

    Update PersonList.js

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -7,7 +7,7 @@ export default class PersonList extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://q76xnqzf38.execute-api.us-west-2.amazonaws.com/prod/get-person`)
+    axios.get(`https://j8kti2vfud.execute-api.us-west-2.amazonaws.com/deploy`)
       .then(res => {
         const persons = res.data.body;
         this.setState({ persons });
